58c3d307013db9f72cb178aa35e9f7fc3ea339a3
Haasini Police <hpolice5296@stu.d214.org>
2024-09-16 09:11:37 -0700

    I changed the data types and changed the strings to objects.

--- ContentView.swift
+++ ContentView.swift
@@ -1,11 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tasks =  ["Mow the lawn", "Feed the dog","Grocery shop"]
+    @State var Array: [Task] =  [(GitHubPractice3.Task(nameOfTask: "Move the Lawn")), GitHubPractice3.Task(nameOfTask: "Feed the Dog"), GitHubPractice3.Task(nameOfTask: "Grocery Shop")]
     var body: some View {
-        List (tasks, id: \.self){
-            task in
-            Text(task)
+        List(Array, id: \.self){
+            Array in
+            Text(Array.nameOfTask)
         }
         VStack {
             Image(systemName: "globe")
