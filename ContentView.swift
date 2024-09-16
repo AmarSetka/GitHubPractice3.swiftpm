e9abfd8e33991a5044e2e0a507435d7f3ac8ee7f
Amar <asetka7368@stu.d214.org>
2024-09-16 10:30:10 -0500

    made the list

--- ContentView.swift
+++ ContentView.swift
@@ -1,7 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tasks =  ["Mow the lawn", "Feed the dog","Grocery shop"]
     var body: some View {
+        List (tasks, id: \.self){
+            task in
+            Text(task)
+        }
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
