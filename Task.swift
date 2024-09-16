2275de8abb98c8164bc4e82a6ce0fea398d92d35
Haasini Police <hpolice5296@stu.d214.org>
2024-09-16 08:31:44 -0700

    I created a new source file and made a variable.

---
+++ Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  GitHubPractice3
+//
+//  Created by Haasini Kala R. Police on 9/16/24.
+//
+
+import Foundation
+
+struct Task: Hashable {
+    @State var nameOfTask: String
+    var body: some View {
+        
+        
+    }
+}
